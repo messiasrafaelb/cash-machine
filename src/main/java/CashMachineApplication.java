99800c612031e3fcfd05ab3aaf8c4707d62e0da5
Rafael <messiasrafaelb@gmail.com>
2026-06-15 23:48:38 -0300

    Implementa ponto de entrada da aplicação

--- src/main/java/CashMachineApplication.java
+++ src/main/java/CashMachineApplication.java
@@ -1,5 +1,43 @@
+import java.util.Scanner;
+
+import controller.dto.BankAccountResponse;
+import controller.dto.BankResponse;
+import view.BankAccountView;
+import view.BankView;
+
 public class CashMachineApplication {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        BankView bankView = new BankView();
+        BankAccountView accountView = new BankAccountView();
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            boolean running = true;
+
+            while (running) {
+                BankResponse selectedBank = bankView.selectBank(scanner);
+
+                if (selectedBank == null) {
+                    System.out.println("\nObrigado por utilizar o Caixa Eletrônico Multibanco. Até mais!");
+                    running = false;
+                    continue;
+                }
+
+                boolean backToBankSelection = false;
+
+                while (!backToBankSelection) {
+                    BankAccountResponse loggedAccount = accountView.showAuthMenu(scanner, selectedBank);
+
+                    if (loggedAccount == null) {
+                        backToBankSelection = true;
+                    } else {
+                        accountView.showTransactionMenu(scanner, loggedAccount);
+                        backToBankSelection = true;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.err.println("\nOcorreu um erro inesperado no sistema: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
